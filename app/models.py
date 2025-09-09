82eedee1cd456e1d117017b3cbb2d8f5d4f067b1
tjonac <tjonathan444@gmail.com>
2025-09-09 14:20:27 -0600

    add models

--- app/models.py
+++ app/models.py
@@ -2,10 +2,22 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
-
+from datetime import datetime
 
 class Calls(SQLModel, table=True):
     id: int | None = Field(primary_key=True)
     operator: str = Field(index=True)
     client_id: int | None = Field(index=True)
-    transcript: str
+    transcript: str | None = None
+    timestamp: datetime = Field(default_factory=datetime.now)
+    sentiment: str | None = None
+    call_target: str | None = None
+
+class Reports(SQLModel, table=True):
+    id: int | None = Field(primary_key=True)
+    call_id: int | None = Field(index=True)
+    client_id: int | None = Field(index=True)
+    topic: str | None = None
+    summary: str | None = None
+    priority: int | None = None
+    timestamp: datetime = Field(default_factory=datetime.now)
